a6449d25c663c48552ae98d4aff2fcc090275d68
岡本紘治 <>
2024-07-09 19:26:20 +0900

    スタートライン。francinetteできる

--- Makefile
+++ Makefile
@@ -6,42 +6,34 @@
 #    By: kokamoto <kokamoto@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/06/05 21:40:05 by kokamoto          #+#    #+#              #
-#    Updated: 2024/06/05 21:40:06 by kokamoto         ###   ########.fr        #
+#    Updated: 2024/07/09 19:23:15 by kokamoto         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+NAME = libftprintf.a
 CC = cc
 CFLAGS = -Wall -Werror -Wextra
-
 SRCDIR = .
 LIBDIR = libft
-NAME = libftprintf.a
-
-$(NAME): $(SRC) $(HDR) $(LIB)
-	$(MAKE) -C $(LIBDIR)
-	cp $(LIB) $(NAME)
-
-$PLACEHOLDER$ : $(NAME)
 
 SRC = $(SRCDIR)/ft_printf.c
-HDR = $(SRCDIR)/ft_printf.h
 LIB = $(LIBDIR)/libft.a
 
-all: program
+all: $(NAME)
 
-program: $(SRC) $(HDR) $(LIB)
-	$(CC) $(CFLAGS) -o $@ $< -L$(LIBDIR) -lft
+$(NAME): $(SRC) $(LIB)
+	$(CC) $(CFLAGS) -I $(LIBDIR) -c $(SRC)
+	ar rc $(NAME) ft_printf.o $(LIBDIR)/*.o
 
 $(LIB):
-	$(MAKE) -C $(LIBDIR)
+	make -C $(LIBDIR)
 
 clean:
-	$(MAKE) -C $(LIBDIR) clean
+	rm -f ft_printf.o
+	make -C $(LIBDIR) fclean
 
 fclean: clean
-	$(MAKE) -C $(LIBDIR) fclean
-	rm -f program
+	rm -f $(NAME)
+	make -C $(LIBDIR) fclean
 
 re: fclean all
-
-.PHONY: all clean fclean re
--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: kokamoto <kokamoto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/28 11:24:56 by kokamoto          #+#    #+#             */
-/*   Updated: 2024/06/06 07:02:24 by kokamoto         ###   ########.fr       */
+/*   Updated: 2024/07/09 19:21:07 by kokamoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,89 +16,87 @@ void	ft_print_str(const char **start, const char **format, int *n)
 {
 	while (**format && **format != '%')
 	{
-		ft_putchar_fd(1, **format);
+		ft_putchar_fd(**format, 1);
 		(*format)++;
 		(*n)++;
 	}
 	*start = *format;
 }
 
-int	ft_strchr_n(const char *s, int c)
+char	ft_strchr_n(const char *s, int c)
 {
 	int	i;
 
 	i = 0;
-    if (c == 0)
-        return (-1);
-	while (*(s + i) && *(s + i) != c)
-        i++;
-	if (*(s + i) == c)
-			return (i);
+	while (*(s + i))
+	{
+		if (*(s + i) == c)
+			return (s[i]);
+		i++;
+	}
 	return (-1);
 }
 
-int	ft_substr_num(const char **format, va_list *args, int mode, t_flags *flags)
+void	ft_process_not_number(int *n, va_list args, char spe)
 {
-	int	num;
-
-	num = -1;
-	if (**format == '*')
+	char *str;
+	if (spe == 'c')
 	{
-		num = va_arg(*args, int); //引数を取得
-		if (num < 0 && !mode) //フィールドの場合は負の値を無視する
-		{
-			num *= -1;
-			flags->flag[1] = 1; //フィールドが負の値の場合は左寄せにする
-		}
-		else if (num < 0)//精度の場合はnumを-1にする
-			num = -1;
-		(*format)++;
+		ft_putchar_fd(va_arg(args, int), 1);
+		(*n)++;
 	}
-	else if (mode || ft_isdigit(**format)) //精度の場合は0が来る場合もあるのでmodeで判定. modeの意味は精度かどうかのフラグ
+	if (spe == 's')
 	{
-		num = 0;
-		while (ft_isdigit(**format))
-			num = num * 10 + (*((*format)++) - '0'); //(*((*format)++)は複雑だが、一つ一つ説明すると、formatの指す先の値を取得してformatを一つ進める。
+		str = va_arg(args, char *);
+		ft_putstr_fd(str, 1);
+		(*n) += ft_strlen(str);
 	}
-	return (num);
+	if (spe == 'p')
+		(*n) += ft_putpointer_fd(va_arg(args, void *), 1);
+	if (spe == '%')
+	{
+		ft_putchar_fd('%', 1);
+		(*n)++;
+	}
 }
 
-void	ft_init_flag(t_flags *flags, int n)
+void	ft_process_number(int *n, va_list args, char spe)
 {
-	flags->flag[0] = 0; // flag[0]の意味は#かどうかのフラグ
-	flags->flag[1] = 0; // flag[1]の意味は左寄せかどうかのフラグ
-	flags->field = -1;  // fieldやaccuracyは0が来る場青もあるので-1で初期化。0だと0が来たときに区別がつかない。
-	flags->accuracy = -1;
-	flags->putnum = (n < 0 ? 0 : n); // putnumはnが負の値の場合は0にする。
-	flags->putlen = 0;
+	long long	num;
+
+	num = va_arg(args, int);
+	if (spe == 'd' || spe == 'i')
+		ft_putint_fd(num, 1);
+	if (spe == 'u')
+		ft_putunsignedint_fd(num, 1);
+	if (spe == 'x')
+		ft_put_uint_hex_fd(num, 1, 'x');
+	if (spe == 'X')
+		ft_put_uint_hex_fd(num, 1, 'X');
+	(*n) += ft_numlen(num);
 }
-void	ft_process(const char **start, const char **format, int *n,
-		va_list args)
+
+void	ft_process(const char **start, const char **format, int *n, va_list args)
 {
-	int		num;
-	t_flags	flags;
+	char	spe;
 
 	(*format)++;
-	ft_init_flag(&flags, *n);
-	while ((num = ft_strchr_n("#0", **format)) >= 0) //フラグの処理 
-		//フラグが来る場合はフラグを立てる
+	spe = ft_strchr_n("cspdiuxX%", **format);
+	if (spe == -1) //変換指定子が見つからない場合
 	{
-		flags.flag[num] = 1;
-		(*format)++;
+		(*format) = *start;
+		return ;
 	}
-	flags.field = ft_substr_num(format, args, 0, &flags); //フィールドの処理
-	if (**format == '.')                                  //精度の処理
-	{
-		(*format)++;
-		flags.accuracy = ft_substr_num(format, args, 1, &flags);
-	}
-	flags.specifier = ft_strchr_n("diuxXefgcspn", **format); //変換指定子の処理
-    *n = ((*n == 0 || num >= 0) ? num : *n); //フラグが来る場合はnをnumにする
+	if (ft_strchr_n("csp%", spe) != -1)
+		ft_process_not_number(n, args, spe);
+	else
+		ft_process_number(n, args, spe);
+	(*format)++;
 }
 
 int	ft_printf(const char *format, ...)
 {
-	va_list		args;
+	va_list	args;
 	int			n;
 	const char	*start;
 
@@ -118,68 +116,53 @@ int	ft_printf(const char *format, ...)
 	return (n);
 }
 
-// int ft_printf(const char *format, ...)
+// int	main(void)
 // {
-//     va_list args;
-//     va_start(args, format);
-//     int len = 0;
-//     while (*format)
-//     {
-//         if (*format == '%')
-//         {
-//             format++;
-//             if (*format == 'c')
-//             {
-//                 char c = va_arg(args, int);
-//                 len += ft_putchar(c);
-//             }
-//             else if (*format == 's')
-//             {
-//                 char *str = va_arg(args, char *);
-//                 len += ft_putstr(str);
-//             }
-//             else if (*format == 'd' || *format == 'i')
-//             {
-//                 int n = va_arg(args, int);
-//                 len += ft_putnbr(n);
-//             }
-//             else
-//             {
-//                 ft_putchar('%');
-//                 len++;
-//             }
-//         }
-//         else
-//         {
-//             len += ft_putchar(*format);
-//         }
-//         format++;
-//     }
-//     va_end(args);
-//     return (len);
+// 	// compare various printf and ft_printf that use the same string or integer. print the function name before the output.
+// 	printf("変換指定子がない場合\n");
+// 	printf(" %d\n", printf("hello"));
+// 	printf(" %d\n\n", ft_printf("hello"));
+	
+// 	printf("変換指定子がcの場合\n");
+// 	printf(" %d\n", printf("c = %c", 'a'));
+// 	printf(" %d\n\n", ft_printf("c = %c", 'a'));
+	
+// 	printf("変換指定子がsの場合\n");
+// 	printf(" %d\n", printf("s = %s", "hello"));
+// 	printf(" %d\n\n", ft_printf("s = %s", "hello"));
+	
+// 	printf("変換指定子がpの場合\n");
+// 	printf(" %d\n", printf("p = %p\n", "hello"));
+// 	printf(" %d\n\n", ft_printf("p = %p\n", "hello"));
+
+// 	printf("変換指定子がdの場合\n");
+// 	printf(" %d\n", printf("d = %d", 42));
+// 	printf(" %d\n\n", ft_printf("d = %d", 42));
+
+// 	printf("変換指定子がiの場合\n");
+// 	printf(" %d\n", printf("i = %i", 42));
+// 	printf(" %d\n\n", ft_printf("i = %i", 42));
+
+// 	printf("変換指定子がuの場合\n");
+// 	printf(" %d\n", printf("u = %u", 42));
+// 	printf(" %d\n\n", ft_printf("u = %u", 42));
+
+// 	printf("変換指定子がxの場合\n");
+// 	printf(" %d\n", printf("x = %x", 42));
+// 	printf(" %d\n\n", ft_printf("x = %x", 42));
+	
+// 	printf("変換指定子がXの場合\n");
+// 	printf(" %d\n", printf("X = %X", 42));
+// 	printf(" %d\n\n", ft_printf("X = %X", 42));
+
+// 	printf("変換指定子が%%の場合\n");
+// 	printf(" %d\n", printf("%%"));
+// 	printf(" %d\n\n", ft_printf("%%"));
+// 	return (0);
 // }
 
 int	main(void)
 {
-	// compare various printf and ft_printf that use the same string or integer. print the function name before the output.
-	printf("printf: %d\n", 42);
-	ft_printf("ft_printf: %d\n", 42);
-	printf("printf: %s\n", "Hello, World!");
-	ft_printf("ft_printf: %s\n", "Hello, World!");
-	printf("printf: %c\n", 'c');
-	ft_printf("ft_printf: %c\n", 'c');
-	printf("printf: %p\n", "Hello, World!");
-	ft_printf("ft_printf: %p\n", "Hello, World!");
-	printf("printf: %x\n", 42);
-	ft_printf("ft_printf: %x\n", 42);
-	printf("printf: %X\n", 42);
-	ft_printf("ft_printf: %X\n", 42);
-	printf("printf: %u\n", 42);
-	ft_printf("ft_printf: %u\n", 42);
-	printf("printf: %i\n", 42);
-	ft_printf("ft_printf: %i\n", 42);
-	printf("printf: %d\n", 42);
-	ft_printf("ft_printf: %d\n", 42);
-
-	return (0);
+	printf("%d\n", printf("%p\n", NULL));
+	printf("%d\n", ft_printf("%p\n", NULL));
 }
